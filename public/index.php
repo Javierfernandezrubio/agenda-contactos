46fe589ef1ca1e4de1fb17e270aa061053373902
Javier Fernandez Rubio <a19feruja@iesgrancapitan.org>
2022-02-02 13:54:47 +0100

    feat: añade la estructura de carpetas y archivos necesarios

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require '../bootstrap.php';
+
+
+echo "<h1>Agenda de contactos</h1>";
+
+echo DBHOST;
+echo "<br>";
+echo DBUSER;
+echo "<br>";
+echo DBPASS;
+echo "<br>";
+echo DBPORT;
+echo "<br>";
+echo DBNAME;
+echo "<br>";
